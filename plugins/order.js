1281a6c84d2084d3c888743371882db49e44ffd5
Monte Williams <monte.williams@gmail.com>
2017-04-22 10:47:34 -0400

    Attempt at stores feature

--- plugins/order.js
+++ plugins/order.js
@@ -1,3 +1,5 @@
+const pizzapi = require('dominos')
+
 module.exports = {
   init: (controller) => {
     controller.hears([/I want a pizza/], ['direct_message', 'direct_mention'], (bot, message) => {
@@ -9,3 +11,7 @@ module.exports = {
     text: `Say "I want pizza" and I'll search for pizza`
   }
 }
+
+let stores = pizzapi.Util.findNearbyStores('11 Times Square, New York, NY 10036', 'Delivery', function(storeData) {
+  console.log("Stores ", storeData)
+})
